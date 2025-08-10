78717c91a969eeafd50d6ab22b3f834d124ae2c7
DilsonZM <dilsonzm@gmail.com>
2025-08-10 15:58:20 -0500

    Add

---
+++ 02_flow_control/01_if.py
@@ -0,0 +1,18 @@
+###
+# Sentencias condicionales if, elif e else
+# Permiten ejecutar bloques de código basados en condiciones
+###
+
+# Ejemplo de uso de if, elif y else
+
+print("Sentencia simple condicional")
+
+edad = input("¿Cuál es tu edad?:")
+edad = int(edad)
+
+#saber si es mayor de edad
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+    
